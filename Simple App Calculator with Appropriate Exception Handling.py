51a66d012ff74a3cfd23a8a979c82a4db4ed03b4
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-07 13:57:07 +0800

    Title

--- Simple App Calculator with Appropriate Exception Handling.py
+++ Simple App Calculator with Appropriate Exception Handling.py
@@ -1,3 +1,9 @@
 #Alexza Jean R. Catanoy
 #BSCPE 1-4
-#Simple App Calculator with Appropriate Exception Handling
+#Exception Handling
+
+#Title
+print("\033[0;36m*" * 90)
+print("\033[1;97mA Simple App Calculator with Appropriate Exception Handling".center(97))
+print("\033[0;36m*" * 90)
+
