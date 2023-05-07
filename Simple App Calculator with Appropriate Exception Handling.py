07535b6a434f0cc4c5ff452a2f674633559ffa0f
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-07 14:03:05 +0800

    Course and Section

--- Simple App Calculator with Appropriate Exception Handling.py
+++ Simple App Calculator with Appropriate Exception Handling.py
@@ -8,3 +8,4 @@ print("\033[1;97mA Simple App Calculator with Appropriate Exception Handling".ce
 print("\033[0;36m*" * 90)
 
 print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4")
